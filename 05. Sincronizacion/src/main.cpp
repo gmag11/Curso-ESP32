3c5d17f105e5017be00a04370ad0a0a1047173f3
Germán Martín <github@gmartin.net>
2022-01-22 22:24:40 +0100

    Práctica 5 - Sincronización

---
+++ 05. Sincronizacion/src/main.cpp
@@ -0,0 +1,82 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <ESPNtpClient.h>
+
+#if __has_include("wificonfig.h")
+#include "wificonfig.h"
+#else
+constexpr auto SSID = "SSID";
+constexpr auto PASSWORD = "PASSWORD";
+#endif
+
+// constexpr auto LED = 5;
+constexpr auto LED = 19;
+// constexpr auto LED_ON = LOW;
+constexpr auto LED_ON = HIGH;
+
+TaskHandle_t tareaLED = NULL;
+TaskHandle_t tareaMensaje = NULL;
+
+constexpr auto periodoLED = 1000;
+constexpr auto ledEncendido = 50; // ms
+constexpr auto ledApagado = 150; // ms
+
+constexpr auto esperaMensaje = 10000;
+
+constexpr auto framerate = 20;
+constexpr auto frametime = 1000 / framerate;
+
+/*
+    El LED se enciende durante 10 ms dos veces, con otros 10 ms de espera entre cada uno. Se repite cada 1 segundo
+*/
+
+void parpadeaLED (void* pvParameters) {
+    for (;;) {
+        time_t start = millis (); // toma la referencia del inicio
+
+        //--------------------- Procesado visual -------------------------------------------
+        time_t ciclo = millis () % periodoLED;
+
+        if ((ciclo > 0 && ciclo < ledEncendido) || (ciclo > ledEncendido + ledApagado && ciclo < ledEncendido * 2 + ledApagado)) {
+            digitalWrite (LED, LED_ON);
+        } else {
+            digitalWrite (LED, !LED_ON);
+        }
+        //----------------------------------------------------------------------------------
+        
+        delay (frametime - (millis () - start)); // Esperar el tiempo necesario para 40 fps
+    }
+}
+
+void escribeMensaje (void* pvParameters) {
+    for (;;) {
+        log_printf ("\nHola mundo. Ya estoy en Internet\n");
+        log_printf ("Mi IP es %s\n", WiFi.localIP ().toString ().c_str ());
+        log_printf ("Sabes qué hora es?... %s\n", NTP.getTimeDateString());
+        delay (esperaMensaje);
+    }
+}
+
+void setup () {
+    Serial.begin (9600);
+    pinMode (LED, OUTPUT);
+    digitalWrite (LED, !LED_ON);
+    WiFi.mode (WIFI_STA);
+    WiFi.begin (SSID, PASSWORD);
+
+    while (WiFi.status () != WL_CONNECTED) {
+        delay (100);
+        Serial.print (".");
+    }
+    NTP.begin ("es.pool.ntp.org", false);
+    NTP.setTimeZone (TZ_Europe_Madrid);
+    NTP.onNTPSyncEvent ([] (NTPEvent_t ntpEvent) {
+        log_printf ("NTP Event: %d: %s\n", ntpEvent, NTP.ntpEvent2str(ntpEvent));
+    });
+
+    xTaskCreate (parpadeaLED, "LED", configMINIMAL_STACK_SIZE, NULL, 1, &tareaLED);
+    xTaskCreate (escribeMensaje, "Mensaje", 2048, NULL, 1, &tareaMensaje);
+}
+
+void loop () {
+}
