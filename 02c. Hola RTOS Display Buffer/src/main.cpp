91143a55e3c15adfcac1291eaa6aff0fe0e7aae0
Germán Martín <github@gmartin.net>
2022-05-23 19:34:41 +0200

    Añadir lección 2c

---
+++ 02c. Hola RTOS Display Buffer/src/main.cpp
@@ -0,0 +1,64 @@
+#include <Arduino.h>
+#include <M5StickCPlus.h>
+
+TaskHandle_t tareaLED = NULL;
+TaskHandle_t tareaMensaje = NULL;
+
+constexpr auto esperaLED = 300;
+constexpr auto esperaMensaje = 1000;
+int counter = 0;
+bool sendMessage = false;
+
+TFT_eSprite Disbuff = TFT_eSprite (&M5.Lcd);
+
+void parpadeaLED (void* pvParameters) {
+    for (;;)   {
+        digitalWrite (BUILTIN_LED, HIGH);
+        delay (esperaLED);
+        digitalWrite (BUILTIN_LED, LOW);
+        delay (esperaLED);
+    }
+}
+
+void escribeMensaje (void* pvParameters) {
+    for (;;) {
+        sendMessage = true;
+        delay (esperaMensaje);
+    }
+}
+
+void setup () {
+    Serial.begin (115200);
+    M5.begin ();
+    pinMode (BUILTIN_LED, OUTPUT);
+    M5.Lcd.setRotation (3);
+    Disbuff.createSprite (240, 135);
+    Disbuff.setRotation (3);
+    delay (100);
+    xTaskCreate (parpadeaLED, "LED", configMINIMAL_STACK_SIZE, NULL, 1, &tareaLED);
+    xTaskCreate (escribeMensaje, "Mensaje", configMINIMAL_STACK_SIZE, NULL, 1, &tareaMensaje);
+}
+
+void updateDisplay () {
+    Disbuff.fillSprite (BLUE);
+    Disbuff.setCursor (10, 10);
+    Disbuff.setTextColor (WHITE);
+    Disbuff.setTextSize (7);
+    uint seconds = millis () / 1000;
+    uint minutes = seconds / 60;
+    seconds = seconds % 60;
+    Disbuff.printf ("%02d:%02d", minutes, seconds, counter);
+    Disbuff.setCursor (10, 65);
+    Disbuff.setTextSize (3);
+    Disbuff.printf ("%.2f V", M5.Axp.GetBatVoltage ());
+    Disbuff.setCursor (10, 90);
+    Disbuff.printf ("%.2f mA", M5.Axp.GetBatCurrent ());
+    Disbuff.pushSprite (0, 0);
+}
+
+void loop () {
+    if (sendMessage) {
+        sendMessage = false;
+        updateDisplay ();
+    }
+}
