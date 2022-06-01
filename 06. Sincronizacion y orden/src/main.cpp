095ea005ba20317642f3a4a7e12e7c30c8655faa
Germán Martín <github@gmartin.net>
2022-06-01 18:06:41 +0200

    Revisar lección 6

--- 06. Sincronizacion y orden/src/main.cpp
+++ 06. Sincronizacion y orden/src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <ESPNtpClient.h>
+#include <M5StickCPlus.h>
 
 #if __has_include("wificonfig.h")
 #include "wificonfig.h"
@@ -10,83 +11,143 @@ constexpr auto PASSWORD = "PASSWORD";
 #endif
 
 // constexpr auto LED = 5;
-constexpr auto LED = 19;
-constexpr auto LED_ON = HIGH;
+constexpr auto LED = 10;
+constexpr auto LED_ON = LOW;
 
 TaskHandle_t tareaLED = NULL;
 TaskHandle_t tareaMensaje = NULL;
+TimerHandle_t tareaDisplay = NULL;
 
-constexpr auto periodoLED = 1000;
-constexpr auto ledEncendido = 50; // ms
-constexpr auto ledApagado = 150; // ms
+TFT_eSprite Disbuff = TFT_eSprite (&M5.Lcd);
 
-constexpr auto esperaMensaje = 10000;
 
-constexpr auto framerate = 20;
-constexpr auto frametime = 1000 / framerate;
+void updateDisplay (void* pvParameters) {
+    constexpr auto periodoDisplay = 10;
 
-bool flash = false;
+    static time_t lastDisplayUpdate = 0;
+    static time_t lastMeasurement = 0;
+    static float current;
+    static float voltage;
+    static String ssid;
+    static String localip;
 
-/*
-    El LED se enciende durante 10 ms dos veces, con otros 10 ms de espera entre cada uno. Se repite cada 1 segundo
-*/
-
-void parpadeaLED (void* pvParameters) {
-    for (;;) {
-        time_t start = millis (); // toma la referencia del inicio
+    if (millis () - lastMeasurement > 1000) {
+        lastMeasurement = millis ();
+        current = M5.Axp.GetBatCurrent ();
+        voltage = M5.Axp.GetBatVoltage ();
+        ssid = WiFi.SSID ();
+        localip = WiFi.localIP ().toString ();
+    }
 
         //--------------------- Procesado visual -------------------------------------------
+    if (millis () - lastDisplayUpdate >= periodoDisplay) {
+        lastDisplayUpdate = millis ();
+        Disbuff.fillSprite (BLACK);
+        Disbuff.setCursor (10, 10);
+        Disbuff.setTextColor (WHITE);
+        Disbuff.setTextSize (4);
+        time_t hora_actual = time (NULL);
+        tm* hora = localtime (&hora_actual);
+        Disbuff.printf ("%02d:%02d:%02d", hora->tm_hour, hora->tm_min, hora->tm_sec);
+        Disbuff.setCursor (10, 50);
+        Disbuff.setTextSize (2);
+        Disbuff.setTextColor (RED);
+        Disbuff.printf ("%.2f V %.2f mA", voltage, current);
+        Disbuff.setCursor (10, 75);
+        Disbuff.setTextColor (WHITE);
+        Disbuff.printf ("%s", ssid.c_str ());
+        Disbuff.setCursor (10, 100);
+        Disbuff.printf ("IP: %s", localip.c_str ());
+        Disbuff.pushSprite (0, 0);
+    }
+    //----------------------------------------------------------------------------------
+}
+
+void parpadeaLED (void* pvParameters) {
+    const int letFlashes = 2;
+    const int ledONtime = 40;
+    const int ledOFFtime = 100;
+    const int ledCyclePeriod = 1000;
+
+
+    for (;;) {
         time_t ciclo;
+        static bool flashed = false;
 
         if (NTP.syncStatus () == syncd) {
-            ciclo = NTP.millis () % periodoLED;;
+            ciclo = NTP.millis () % ledCyclePeriod;;
         } else {
-            ciclo = millis () % periodoLED;
+            ciclo = millis () % ledCyclePeriod;
         }
-
-        if (flash) {
-            if ((ciclo > 0 && ciclo < ledEncendido) || (ciclo > ledEncendido + ledApagado && ciclo < ledEncendido * 2 + ledApagado)) {
+        
+        if ((!flashed) && (ciclo >= 0 && ciclo < (ledONtime + ledOFFtime)*letFlashes)) {
+            flashed = true;
+            for (int i = 0; i < letFlashes; i++) {
                 digitalWrite (LED, LED_ON);
-            } else {
+                delay (ledONtime);
                 digitalWrite (LED, !LED_ON);
+                delay (ledOFFtime);
             }
         }
-        //----------------------------------------------------------------------------------
-        
-        delay (frametime - (millis () - start)); // Esperar el tiempo necesario para 40 fps
+
+        if (flashed && (ciclo > (ledONtime + ledOFFtime) * letFlashes)) {
+            flashed = false;
+        }
+        delay (1);
     }
 }
 
 void escribeMensaje (void* pvParameters) {
+    constexpr auto esperaMensaje = 10000;
+    
     for (;;) {
         log_printf ("\nHola mundo. Ya estoy en Internet\n");
         log_printf ("Mi IP es %s\n", WiFi.localIP ().toString ().c_str ());
         log_printf ("Sabes qué hora es?... %s\n", NTP.getTimeDateString());
+
         delay (esperaMensaje);
     }
 }
 
 void setup () {
-    Serial.begin (9600);
+    Serial.begin (115200);
     pinMode (LED, OUTPUT);
     digitalWrite (LED, !LED_ON);
+
     WiFi.mode (WIFI_STA);
+    //WiFi.enableLongRange (false);
     WiFi.begin (SSID, PASSWORD);
+
+    M5.begin ();
+    M5.Lcd.setRotation (3);
+    Disbuff.createSprite (240, 135);
+    Disbuff.setRotation (3);
+    Disbuff.fillSprite (BLACK);
+    Disbuff.setTextColor (WHITE);
+    Disbuff.setTextSize (2);
+    Disbuff.setCursor (10, 10);
+    Disbuff.printf ("Conectando");
+    Disbuff.pushSprite (0, 0);
+    delay (100);
 
     while (WiFi.status () != WL_CONNECTED) {
         delay (100);
         Serial.print (".");
     }
-    NTP.begin ("es.pool.ntp.org", false);
+    //WiFi.setAutoReconnect (false);
+
+    NTP.begin ("192.168.5.120", false);
     NTP.setTimeZone (TZ_Europe_Madrid);
     NTP.onNTPSyncEvent ([] (NTPEvent_t ntpEvent) {
-        log_printf ("NTP Event: %d: %s\n", ntpEvent, NTP.ntpEvent2str(ntpEvent));
-    });
-    
-    xTaskCreate (parpadeaLED, "LED", 1512, NULL, 1, &tareaLED);
+        log_printf ("NTP Event: %d: %s\n", ntpEvent, NTP.ntpEvent2str (ntpEvent));
+                        });
+
     xTaskCreate (escribeMensaje, "Mensaje", 2048, NULL, 1, &tareaMensaje);
+
+    xTaskCreate (parpadeaLED, "LED", 2048, NULL, 1, &tareaLED);
+
+    tareaDisplay = xTimerCreate ("Display", 40 / portTICK_PERIOD_MS, pdTRUE, NULL, updateDisplay);
+    xTimerStart (tareaDisplay, 0);
 }
 
-void loop () {
-    flash = (NTP.syncStatus() == syncd);
-}
+void loop () {}
