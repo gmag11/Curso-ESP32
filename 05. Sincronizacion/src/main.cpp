72d2db7c305edff08e24900e8a13581af559f805
Germán Martín <github@gmartin.net>
2022-06-01 17:17:12 +0200

    Revisar lección 5

--- 05. Sincronizacion/src/main.cpp
+++ 05. Sincronizacion/src/main.cpp
@@ -2,9 +2,7 @@
 #include <WiFi.h>
 #include <ESPNtpClient.h>
 
-#ifdef ARDUINO_M5Stick_C
 #include <M5StickCPlus.h>
-#endif // ARDUINO_M5Stick_C
 
 #if __has_include("wificonfig.h")
 #include "wificonfig.h"
@@ -13,96 +11,89 @@ constexpr auto SSID = "SSID";
 constexpr auto PASSWORD = "PASSWORD";
 #endif
 
-#ifdef ARDUINO_M5Stick_C
+// constexpr auto LED = 5;
 constexpr auto LED = 10;
 constexpr auto LED_ON = LOW;
-#else
-constexpr auto LED = 5;
-constexpr auto LED_ON = LOW;
-#endif // ARDUINO_M5Stick_C
 
-
-
-TimerHandle_t ledTimer;
+TaskHandle_t tareaLED = NULL;
 TaskHandle_t tareaMensaje = NULL;
+TimerHandle_t tareaDisplay = NULL;
 
-constexpr auto periodoLED = 1000;
-constexpr auto ledEncendido = 50; // ms
-constexpr auto ledApagado = 150; // ms
-
-constexpr auto esperaMensaje = 1000;
-
-constexpr auto framerate = 4;
-constexpr auto frametime = 1000 / framerate;
-
-#ifdef ARDUINO_M5Stick_C
 TFT_eSprite Disbuff = TFT_eSprite (&M5.Lcd);
-#endif // ARDUINO_M5Stick_C
 
-/*
-    El LED se enciende durante 10 ms dos veces, con otros 10 ms de espera entre cada uno. Se repite cada 1 segundo
-*/
+
+void updateDisplay (void* pvParameters) {
+    constexpr auto periodoDisplay = 10;
+
+    static time_t lastDisplayUpdate = 0;
+    static time_t lastMeasurement = 0;
+    static float current;
+    static float voltage;
+    static String ssid;
+    static String localip;
+
+    if (millis () - lastMeasurement > 1000) {
+        lastMeasurement = millis ();
+        current = M5.Axp.GetBatCurrent ();
+        voltage = M5.Axp.GetBatVoltage ();
+        ssid = WiFi.SSID ();
+        localip = WiFi.localIP ().toString ();
+    }
+
+        //--------------------- Procesado visual -------------------------------------------
+    if (millis () - lastDisplayUpdate >= periodoDisplay) {
+        lastDisplayUpdate = millis ();
+        Disbuff.fillSprite (BLACK);
+        Disbuff.setCursor (10, 10);
+        Disbuff.setTextColor (WHITE);
+        Disbuff.setTextSize (4);
+        time_t hora_actual = time (NULL);
+        tm* hora = localtime (&hora_actual);
+        Disbuff.printf ("%02d:%02d:%02d", hora->tm_hour, hora->tm_min, hora->tm_sec);
+        Disbuff.setCursor (10, 50);
+        Disbuff.setTextSize (2);
+        Disbuff.setTextColor (RED);
+        Disbuff.printf ("%.2f V %.2f mA", voltage, current);
+        Disbuff.setCursor (10, 75);
+        Disbuff.setTextColor (WHITE);
+        Disbuff.printf ("%s", ssid.c_str ());
+        Disbuff.setCursor (10, 100);
+        Disbuff.printf ("IP: %s", localip.c_str ());
+        Disbuff.pushSprite (0, 0);
+    }
+    //----------------------------------------------------------------------------------
+}
 
 void parpadeaLED (void* pvParameters) {
-    // for (;;) {
-    //     time_t start = millis (); // toma la referencia del inicio
+    const int letFlashes = 2;
+    const int ledONtime = 40;
+    const int ledOFFtime = 100;
+    const int ledCyclePeriod = 1000;
 
-    //     //--------------------- Procesado visual -------------------------------------------
-    //     time_t ciclo = millis () % periodoLED;
+    static time_t lastLEDCycle = 0;
 
-    //     if ((ciclo > 0 && ciclo < ledEncendido) || (ciclo > ledEncendido + ledApagado && ciclo < ledEncendido * 2 + ledApagado)) {
-    //         digitalWrite (LED, LED_ON);
-    //     } else {
-    //         digitalWrite (LED, !LED_ON);
-    //     }
-    //     //----------------------------------------------------------------------------------
-        
-    //     delay (frametime - (millis () - start)); // Esperar el tiempo necesario para 40 fps
-    // }
-
-    log_printf ("Incio LED %u\n", millis());
-    for (int i = 0; i < 2; i++) {
-        digitalWrite (LED, LED_ON);
-        delay (ledEncendido);
-        digitalWrite (LED, !LED_ON);
-        delay (ledApagado);
+    for (;;) {
+        if (millis () - lastLEDCycle > ledCyclePeriod) {
+            lastLEDCycle = millis ();
+            for (int i = 0; i < letFlashes; i++) {
+                digitalWrite (LED, LED_ON);
+                delay (ledONtime);
+                digitalWrite (LED, !LED_ON);
+                delay (ledOFFtime);
+            }
+        }
+        delay (10);
     }
-    log_printf ("Fin LED %d\n", LED);
-    // time_t cicloRestante = millis ();
-    // cicloRestante = periodoLED - cicloRestante % periodoLED;
-    // xTimerStart (ledTimer, cicloRestante / portTICK_PERIOD_MS);
-
 }
-
-#ifdef ARDUINO_M5Stick_C
-void updateDisplay () {
-    Disbuff.fillSprite (BLUE);
-    Disbuff.setTextColor (WHITE);
-    Disbuff.setTextSize (4);
-    time_t hora_actual = time (NULL);
-    tm* hora = localtime (&hora_actual);
-    Disbuff.setCursor (10, 10);
-    Disbuff.printf ("%02d:%02d:%02d", hora->tm_hour, hora->tm_min, hora->tm_sec);
-    Disbuff.setCursor (10, 55);
-    Disbuff.setTextSize (3);
-    Disbuff.printf ("%.2f V", M5.Axp.GetBatVoltage ());
-    Disbuff.setCursor (10, 86);
-    Disbuff.setTextSize (2);
-    Disbuff.printf ("IP: %s", WiFi.localIP ().toString ().c_str ());
-    Disbuff.setCursor (10, 107);
-    Disbuff.printf ("%s", WiFi.SSID ().c_str ());
-    Disbuff.pushSprite (0, 0);
-}
-#endif // ARDUINO_M5Stick_C
 
 void escribeMensaje (void* pvParameters) {
+    constexpr auto esperaMensaje = 10000;
+    
     for (;;) {
         log_printf ("\nHola mundo. Ya estoy en Internet\n");
         log_printf ("Mi IP es %s\n", WiFi.localIP ().toString ().c_str ());
         log_printf ("Sabes qué hora es?... %s\n", NTP.getTimeDateString());
-#ifdef ARDUINO_M5Stick_C
-        updateDisplay ();
-#endif // ARDUINO_M5Stick_C
+
         delay (esperaMensaje);
     }
 }
@@ -110,60 +101,43 @@ void escribeMensaje (void* pvParameters) {
 void setup () {
     Serial.begin (115200);
     pinMode (LED, OUTPUT);
-    digitalWrite (LED, LED_ON);
-    delay (100);
     digitalWrite (LED, !LED_ON);
-    ledTimer = xTimerCreate ("LED", periodoLED / portTICK_PERIOD_MS, pdTRUE, NULL, parpadeaLED);
-    
-    time_t cicloRestante = millis ();
-    cicloRestante = periodoLED - (cicloRestante % periodoLED);
-    vTaskDelay (cicloRestante / portTICK_PERIOD_MS);
-    xTimerStart (ledTimer, pdMS_TO_TICKS (5000));
-    log_printf ("Millis: %u, Ciclo restante %d\n", millis(), cicloRestante);
 
     WiFi.mode (WIFI_STA);
     //WiFi.enableLongRange (false);
     WiFi.begin (SSID, PASSWORD);
 
-#ifdef ARDUINO_M5Stick_C
     M5.begin ();
     M5.Lcd.setRotation (3);
     Disbuff.createSprite (240, 135);
     Disbuff.setRotation (3);
-    Disbuff.fillSprite (BLUE);
+    Disbuff.fillSprite (BLACK);
     Disbuff.setTextColor (WHITE);
     Disbuff.setTextSize (2);
     Disbuff.setCursor (10, 10);
     Disbuff.printf ("Conectando");
     Disbuff.pushSprite (0, 0);
     delay (100);
-#endif // ARDUINO_M5Stick_C
 
     while (WiFi.status () != WL_CONNECTED) {
         delay (100);
         Serial.print (".");
     }
-    WiFi.setAutoReconnect (false);
+    //WiFi.setAutoReconnect (false);
+
     NTP.begin ("es.pool.ntp.org", false);
     NTP.setTimeZone (TZ_Europe_Madrid);
     NTP.onNTPSyncEvent ([] (NTPEvent_t ntpEvent) {
-        log_printf ("NTP Event: %d: %s\n", ntpEvent, NTP.ntpEvent2str(ntpEvent));
-    });
+        log_printf ("NTP Event: %d: %s\n", ntpEvent, NTP.ntpEvent2str (ntpEvent));
+                        });
 
     xTaskCreate (escribeMensaje, "Mensaje", 2048, NULL, 1, &tareaMensaje);
-    //xTaskCreate (parpadeaLED, "LED", 2048, NULL, 1, &tareaLED);
+
+    xTaskCreate (parpadeaLED, "LED", configMINIMAL_STACK_SIZE, NULL, 1, &tareaLED);
+
+    tareaDisplay = xTimerCreate ("Display", 40 / portTICK_PERIOD_MS, pdTRUE, NULL, updateDisplay);
+    xTimerStart (tareaDisplay, 0);
 }
 
 void loop () {
-    //time_t start = millis (); // toma la referencia del inicio
-
-        //--------------------- Procesado visual -------------------------------------------
-    // time_t ciclo = millis () % periodoLED;
-
-    // if ((ciclo > 0 && ciclo < ledEncendido) || (ciclo > ledEncendido + ledApagado && ciclo < ledEncendido * 2 + ledApagado)) {
-    //     digitalWrite (LED, LED_ON);
-    // } else {
-    //     digitalWrite (LED, !LED_ON);
-    // }
-    //----------------------------------------------------------------------------------
 }
