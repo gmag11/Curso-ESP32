b0a273b38ebb5a628881528991479dfeaa8f7efa
Germán Martín <github@gmartin.net>
2022-06-01 14:58:51 +0200

    REvisar lección 3

--- 03. Control de tiempo sin delay/src/main.cpp
+++ 03. Control de tiempo sin delay/src/main.cpp
@@ -3,8 +3,9 @@
 
 constexpr auto LED = 10;
 
-TaskHandle_t tareaLED = NULL;
-TaskHandle_t tareaMensaje = NULL;
+//TaskHandle_t tareaLED = NULL;
+TimerHandle_t tareaLED = NULL;
+TimerHandle_t tareaDisplay = NULL;
 
 constexpr auto periodoLED = 1000;
 constexpr auto ledEncendido = 50; // ms
@@ -13,60 +14,50 @@ constexpr auto ledApagado = 150; // ms
 constexpr auto framerateLED = 20;
 constexpr auto frametimeLED = 1000 / framerateLED;
 
-constexpr auto periodoDisplay = 1000;
+constexpr auto periodoDisplay = 10;
 constexpr auto framerateDisplay = 15;
 constexpr auto frametimeDisplay = 1000 / framerateDisplay;
-
 /*
     El LED se enciende durante 10 ms dos veces, con otros 10 ms de espera entre cada uno. Se repite cada 1 segundo
 */
 
 TFT_eSprite Disbuff = TFT_eSprite (&M5.Lcd);
 
-void parpadeaLED (void* pvParameters) {
-    for (;;) {
-        time_t start = millis (); // toma la referencia del inicio
+void updateDisplay (void* pvParameters = NULL) {
+    static time_t lastDisplayUpdate = 0;
+    static time_t lastMeasurement = 0;
+    static float current;
+    static float voltage;
 
-        //--------------------- Procesado visual -------------------------------------------
-        time_t ciclo = millis () % periodoLED;
-
-        if ((ciclo > 0 && ciclo < ledEncendido) || (ciclo > ledEncendido + ledApagado && ciclo < ledEncendido * 2 + ledApagado)) {
-            digitalWrite (LED, LOW);
-        } else {
-            digitalWrite (LED, HIGH);
-        }
-        //----------------------------------------------------------------------------------
-
-        delay (frametimeLED - (millis () - start)); // Esperar el tiempo necesario para mantener fps
+    if (millis()- lastMeasurement > 1000) {
+        lastMeasurement = millis();
+        current = M5.Axp.GetBatCurrent ();
+        voltage = M5.Axp.GetBatVoltage ();
     }
+
+    if (millis () - lastDisplayUpdate > periodoDisplay) {
+        lastDisplayUpdate = millis ();
+        Disbuff.fillSprite (BLACK);
+        Disbuff.setCursor (10, 10);
+        Disbuff.setTextColor (WHITE);
+        Disbuff.setTextSize (4);
+        time_t currentTime = millis ();
+        uint cents = (currentTime % 1000) / 10;
+        uint seconds = currentTime / 1000;
+        uint minutes = seconds / 60;
+        seconds = seconds % 60;
+        Disbuff.printf ("%02d:%02d.%02d", minutes, seconds, cents);
+        Disbuff.setCursor (10, 55);
+        Disbuff.setTextSize (2);
+        Disbuff.printf ("%.2f V %.2f mA", voltage, current);
+        //Disbuff.setCursor (10, 90);
+        Disbuff.pushSprite (0, 0);
+    }
+
 }
 
-void updateMessage (void* pvParameters) {
-    for (;;) {
-        time_t start = millis (); // toma la referencia del inicio
-        static time_t lastDisplayUpdate = 0;
-
-        //--------------------- Procesado visual -------------------------------------------
-        if (millis () - lastDisplayUpdate >= periodoDisplay) {
-            lastDisplayUpdate = millis ();
-            Disbuff.fillSprite (BLUE);
-            Disbuff.setCursor (10, 10);
-            Disbuff.setTextColor (WHITE);
-            Disbuff.setTextSize (7);
-            uint seconds = millis () / 1000;
-            uint minutes = seconds / 60;
-            seconds = seconds % 60;
-            Disbuff.printf ("%02d:%02d", minutes, seconds);
-            Disbuff.setCursor (10, 65);
-            Disbuff.setTextSize (3);
-            Disbuff.printf ("%.2f V", M5.Axp.GetBatVoltage ());
-            Disbuff.setCursor (10, 90);
-            Disbuff.printf ("%.2f mA", M5.Axp.GetBatCurrent ());
-            Disbuff.pushSprite (0, 0);
-        }
-        //----------------------------------------------------------------------------------
-        vTaskDelay (frametimeDisplay - (millis () - start) / portTICK_PERIOD_MS); // Esperar el tiempo necesario para cumplir fps
-    }
+void parpadeaLED (void* pvParameters) {
+    digitalWrite (LED, !digitalRead (LED));
 }
 
 void setup () {
@@ -83,8 +74,11 @@ void setup () {
     Disbuff.printf ("Hello world!");
     Disbuff.pushSprite (0, 0);
     delay (500);
-    xTaskCreate (parpadeaLED, "LED", configMINIMAL_STACK_SIZE, NULL, 1, &tareaLED);
-    xTaskCreate (updateMessage, "Display", 2048, NULL, 1, &tareaMensaje);
+    //xTaskCreate (parpadeaLED, "LED", configMINIMAL_STACK_SIZE, NULL, 1, &tareaLED);
+    tareaLED = xTimerCreate ("LED", periodoLED / portTICK_PERIOD_MS, pdTRUE, NULL, parpadeaLED);
+    xTimerStart (tareaLED, 0);
+    tareaDisplay = xTimerCreate ("Display", 40 / portTICK_PERIOD_MS, pdTRUE, NULL, updateDisplay);
+    xTimerStart (tareaDisplay, 0);
 }
 
 void loop () {}
