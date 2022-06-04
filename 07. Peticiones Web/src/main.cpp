c5b6020c1d2231aed02a916f69cd46b32b72255f
Germán Martín <github@gmartin.net>
2022-06-04 19:01:54 +0200

    Lección 7

---
+++ 07. Peticiones Web/src/main.cpp
@@ -0,0 +1,141 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <sntp.h>
+#include <M5StickCPlus.h>
+#include <HTTPClient.h>
+
+#if __has_include("wificonfig.h")
+#include "wificonfig.h"
+#else
+constexpr auto SSID = "SSID";
+constexpr auto PASSWORD = "PASSWORD";
+#endif
+
+// constexpr auto LED = 5;
+constexpr auto LED = 10;
+constexpr auto LED_ON = LOW;
+
+TaskHandle_t tareaLED = NULL;
+TaskHandle_t tareaMensaje = NULL;
+TimerHandle_t tareaDisplay = NULL;
+
+bool timeSyncd = false;
+
+TFT_eSprite Disbuff = TFT_eSprite (&M5.Lcd);
+float temp = -200;
+
+
+void updateDisplay (void* pvParameters) {
+    constexpr auto periodoDisplay = 10;
+
+    static time_t lastDisplayUpdate = 0;
+    static time_t lastMeasurement = 0;
+    static float current;
+    static float voltage;
+    static String ssid;
+    static String localip;
+
+    if (millis () - lastMeasurement > 1000) {
+        lastMeasurement = millis ();
+        current = M5.Axp.GetBatCurrent ();
+        voltage = M5.Axp.GetBatVoltage ();
+        ssid = WiFi.SSID ();
+        localip = WiFi.localIP ().toString ();
+    }
+
+        //--------------------- Procesado visual -------------------------------------------
+    if (millis () - lastDisplayUpdate >= periodoDisplay) {
+        lastDisplayUpdate = millis ();
+        Disbuff.fillSprite (BLACK);
+        Disbuff.setCursor (10, 10);
+        Disbuff.setTextColor (WHITE);
+        Disbuff.setTextSize (4);
+        time_t hora_actual = time (NULL);
+        tm* hora = localtime (&hora_actual);
+        Disbuff.printf ("%02d:%02d:%02d", hora->tm_hour, hora->tm_min, hora->tm_sec);
+        Disbuff.setCursor (10, 50);
+        Disbuff.setTextSize (2);
+        Disbuff.setTextColor (RED);
+        Disbuff.printf ("%.2f V %.2f mA", voltage, current);
+        Disbuff.setCursor (10, 75);
+        Disbuff.setTextSize (4);
+        if (temp > -100) {
+            Disbuff.setTextColor (GREEN);
+            Disbuff.printf ("T:%.2f C", temp);
+        } else {
+            Disbuff.setTextColor (RED);
+            Disbuff.printf ("T:--.-- C");
+        }
+
+        Disbuff.pushSprite (0, 0);
+    }
+    //----------------------------------------------------------------------------------
+}
+
+void setup () {
+    Serial.begin (115200);
+    pinMode (LED, OUTPUT);
+    digitalWrite (LED, !LED_ON);
+
+    WiFi.mode (WIFI_STA);
+    //WiFi.enableLongRange (false);
+    WiFi.begin (SSID, PASSWORD);
+
+    M5.begin ();
+    M5.Lcd.setRotation (3);
+    Disbuff.createSprite (240, 135);
+    Disbuff.setRotation (3);
+    Disbuff.fillSprite (BLACK);
+    Disbuff.setTextColor (WHITE);
+    Disbuff.setTextSize (2);
+    Disbuff.setCursor (10, 10);
+    Disbuff.printf ("Conectando");
+    Disbuff.pushSprite (0, 0);
+    delay (100);
+
+    while (WiFi.status () != WL_CONNECTED) {
+        delay (100);
+        Serial.print (".");
+    }
+    //WiFi.setAutoReconnect (false);
+
+
+    sntp_setoperatingmode (SNTP_OPMODE_POLL);
+    sntp_set_sync_mode (SNTP_SYNC_MODE_SMOOTH);
+    sntp_setservername (0, "192.168.5.120");
+    setenv ("TZ", PSTR ("CET-1CEST,M3.5.0,M10.5.0/3"), 1);
+    tzset ();
+    sntp_set_time_sync_notification_cb ([] (struct timeval* tv) {
+        timeSyncd = true;
+        log_printf ("\nHora sincronizada\n");
+    });
+    sntp_init ();
+
+    const uint fps = 10;
+
+    tareaDisplay = xTimerCreate ("Display", pdMS_TO_TICKS(1000/fps), pdTRUE, NULL, updateDisplay);
+    xTimerStart (tareaDisplay, 0);
+}
+
+void loop () {
+    static time_t lastWeatherUpdate = -290000;
+    const time_t wuperiod = 1000 * 60 * 5;
+    const String POBLACION = "Madrid";
+
+    if (millis () - lastWeatherUpdate > wuperiod) {
+        lastWeatherUpdate = millis ();
+        HTTPClient client;
+        client.begin ("http://api.openweathermap.org/data/2.5/weather?units=metric&q=" + POBLACION + "&appid=" + OWM_API_KEY);
+        int httpCode = client.GET ();
+        if (httpCode == 200) {
+            String payload = client.getString ();
+            log_printf ("\n%s\n", payload.c_str ());
+            temp = payload.substring (payload.indexOf ("\"temp\":") + 7, payload.indexOf (",\"feels_like\"")).toFloat();
+            log_printf ("\nTemperatura: %f\n", temp);
+        } else {
+            log_printf ("\nError: %d %s\n", httpCode, client.errorToString (httpCode).c_str ());
+            temp = -200;
+        }
+    }
+    
+}
