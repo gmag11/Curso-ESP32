d8f6e83e4e85447c791e7d9ece203a2fd691a9e7
gmartin <github@gmartin.net>
2022-06-08 16:16:43 +0200

    Revisar lección 8

--- 08. Control remoto/src/main.cpp
+++ 08. Control remoto/src/main.cpp
@@ -1,7 +1,8 @@
 #include <Arduino.h>
 #include <WiFi.h>
-#include <ESPNtpClient.h>
 #include <ESPAsyncWebServer.h>
+#include <sntp.h>
+#include <M5StickCPlus.h>
 
 #if __has_include("wificonfig.h")
 #include "wificonfig.h"
@@ -10,9 +11,16 @@ constexpr auto SSID = "SSID";
 constexpr auto PASSWORD = "PASSWORD";
 #endif
 
-constexpr auto LED = 5;
+constexpr auto LED = 10;
 //constexpr auto LED = 19;
 constexpr auto LED_ON = LOW;
+
+TaskHandle_t tareaMensaje = NULL;
+TimerHandle_t tareaDisplay = NULL;
+
+bool timeSyncd = false;
+
+bool ledOn = false;
 
 AsyncWebServer server (80);
 
@@ -23,6 +31,91 @@ void notFound (AsyncWebServerRequest* request) {
     request->send (404, "text/plain", response);
 }
 
+TFT_eSprite Disbuff = TFT_eSprite (&M5.Lcd);
+
+void updateDisplay (void* pvParameters) {
+    RTC_TimeTypeDef sTime;
+    constexpr auto periodoDisplay = 10;
+
+    static time_t lastDisplayUpdate = 0;
+    static time_t lastMeasurement = 0;
+    static float current;
+    static float voltage;
+    static String ssid;
+    static String localip;
+
+    if (millis () - lastMeasurement > 1000) {
+        lastMeasurement = millis ();
+        current = M5.Axp.GetBatCurrent ();
+        voltage = M5.Axp.GetBatVoltage ();
+        ssid = WiFi.SSID ();
+        localip = WiFi.localIP ().toString ();
+    }
+
+        //--------------------- Procesado visual -------------------------------------------
+    if (millis () - lastDisplayUpdate >= periodoDisplay) {
+        lastDisplayUpdate = millis ();
+        if (ledOn) {
+            Disbuff.fillSprite (BLUE);
+        } else {
+            Disbuff.fillSprite (BLACK);
+        }
+        Disbuff.setCursor (10, 10);
+        Disbuff.setTextColor (WHITE);
+        Disbuff.setTextSize (4);
+        M5.Rtc.GetTime (&sTime);
+        Disbuff.printf ("%02d:%02d:%02d", sTime.Hours, sTime.Minutes, sTime.Seconds);
+        Disbuff.setCursor (10, 50);
+        Disbuff.setTextSize (2);
+        Disbuff.setTextColor (RED);
+        Disbuff.printf ("%.2f V %.2f mA", voltage, current);
+        Disbuff.setCursor (10, 75);
+        Disbuff.setTextColor (WHITE);
+        Disbuff.printf ("%s", ssid.c_str ());
+        Disbuff.setCursor (10, 100);
+        Disbuff.printf ("IP: %s", localip.c_str ());
+        Disbuff.pushSprite (0, 0);
+    }
+    //----------------------------------------------------------------------------------
+}
+
+void escribeMensaje (void* pvParameters) {
+    constexpr auto esperaMensaje = 10000;
+
+    for (;;) {
+        log_printf ("\nHola mundo. Ya estoy en Internet\n");
+        log_printf ("Mi IP es %s\n", WiFi.localIP ().toString ().c_str ());
+        time_t now = time (NULL);
+        log_printf ("Sabes qué hora es?... %s\n", ctime(&now));
+
+        delay (esperaMensaje);
+    }
+}
+
+
+void time_sync_cb (timeval* ntptime) {
+    time_t hora_actual = time (NULL);
+
+    tm* timeinfo = localtime (&hora_actual);
+    RTC_DateTypeDef sDate;
+    RTC_TimeTypeDef sTime;
+
+    sTime.Hours = timeinfo->tm_hour;
+    sTime.Minutes = timeinfo->tm_min;
+    sTime.Seconds = timeinfo->tm_sec;
+
+    M5.Rtc.SetTime (&sTime);
+
+    sDate.WeekDay = timeinfo->tm_wday;
+    sDate.Month = timeinfo->tm_mon + 1;
+    sDate.Date = timeinfo->tm_mday;
+    sDate.Year = timeinfo->tm_year + 1900;
+
+    M5.Rtc.SetData (&sDate);
+
+    timeSyncd = true;
+}
+
 void setup () {
     Serial.begin (9600);
     pinMode (LED, OUTPUT);
@@ -30,30 +123,55 @@ void setup () {
     WiFi.mode (WIFI_STA);
     WiFi.begin (SSID, PASSWORD);
 
-    while (WiFi.status () != WL_CONNECTED) {
-        delay (100);
-        Serial.print (".");
-    }
-    Serial.printf ("\nWiFi Connected. IP address: %s\n", WiFi.localIP ().toString ().c_str ());
-    NTP.begin ("192.168.123.61", false);
-    NTP.setTimeZone (TZ_Europe_Madrid);
-    // NTP.onNTPSyncEvent ([] (NTPEvent_t ntpEvent) {
-    //     log_printf ("NTP Event: %d: %s\n", ntpEvent, NTP.ntpEvent2str (ntpEvent));
-    //                     });
+    M5.begin ();
+    M5.Lcd.setRotation (3);
+    Disbuff.createSprite (240, 135);
+    Disbuff.setRotation (3);
+    Disbuff.fillSprite (BLACK);
+    Disbuff.setTextColor (WHITE);
+    Disbuff.setTextSize (2);
+    Disbuff.setCursor (10, 10);
+    Disbuff.printf ("Conectando");
+    Disbuff.pushSprite (0, 0);
+    delay (100);
+
+    sntp_setoperatingmode (SNTP_OPMODE_POLL);
+    sntp_set_sync_mode (SNTP_SYNC_MODE_SMOOTH);
+    sntp_setservername (0, "192.168.5.120");
+    setenv ("TZ", PSTR ("CET-1CEST,M3.5.0,M10.5.0/3"), 1);
+    tzset ();
+    sntp_set_time_sync_notification_cb (time_sync_cb);
+    sntp_init ();
+
+    xTaskCreate (escribeMensaje, "Mensaje", 2048, NULL, 1, &tareaMensaje);
+
+    const uint fps = 10;
+
+    tareaDisplay = xTimerCreate ("Display", pdMS_TO_TICKS (1000 / fps), pdTRUE, NULL, updateDisplay);
+    xTimerStart (tareaDisplay, 0);
+    
     server.on ("/ledon", HTTP_GET, [] (AsyncWebServerRequest* request) {
         digitalWrite (LED, LED_ON);
-        String response = NTP.getTimeDateStringForJS ();
+        ledOn = true;
+        time_t now = time (NULL);
+        String response = ctime (&now);
+        response.trim ();
         response += ": LED ON";
         request->send (200, "text/plain", response);
                });
     server.on ("/ledoff", HTTP_GET, [] (AsyncWebServerRequest* request) {
         digitalWrite (LED, !LED_ON);
-        String response = NTP.getTimeDateStringForJS ();
+        ledOn = false;
+        time_t now = time (NULL);
+        String response = ctime (&now);
+        response.trim ();
         response += ": LED OFF";
         request->send (200, "text/plain", response);
                });
     server.on ("/led", HTTP_GET, [] (AsyncWebServerRequest* request) {
-        String response = NTP.getTimeDateStringForJS ();
+        time_t now = time (NULL);
+        String response = ctime (&now);
+        response.trim ();
         response += ": LED is";
         response += (digitalRead (LED) == LED_ON) ? " ON" : " OFF";
         request->send (200, "text/plain", response);
