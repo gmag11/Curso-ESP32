be00337e17f47db2acadb9dbb2d9fa3df8436f8d
Germán Martín <github@gmartin.net>
2022-06-23 23:14:26 +0200

    Lección 13

---
+++ 13. Sensores y librerías/src/Filter.cpp
@@ -0,0 +1,254 @@
+/**
+  * @file Filter.cpp
+  * @version 0.9.8
+  * @date 15/07/2021
+  * @author German Martin
+  * @brief Filter to process message rate or other values
+  */
+
+#include "Filter.h"
+#include <QuickDebug.h>
+
+#define FILTER_TAG "FILTER"
+
+float FilterClass::addValue (float value) {
+	switch (_filterType) {
+	case AVERAGE_FILTER:
+		return aveFilter (value);
+		break;
+	case MEDIAN_FILTER:
+		return medianFilter (value);
+		break;
+	default:
+		return value;
+	}
+}
+
+float FilterClass::addWeigth (float coeff) {
+	float sumWeight = 0;
+
+	for (int i = _order - 1; i > 0; i--) {
+		_weightValues[i] = _weightValues[i - 1];
+	}
+	_weightValues[0] = coeff;
+
+	for (int i = 0; i < _order; i++) {
+		sumWeight += _weightValues[i];
+	}
+
+    //DEBUG_VERBOSE (FILTER_TAG, "SumWeight: %f", sumWeight);
+
+	return sumWeight;
+}
+
+
+float FilterClass::aveFilter (float value) {
+	float sumValue = 0;
+	float sumWeight = 0;
+	float procValue;
+	int left, right;
+
+	for (int i = 0; i < _order - 1; i++) {
+		_rawValues[i] = _rawValues[i + 1];
+	}
+	_rawValues[_order - 1] = value;
+
+	DEBUG_VERBOSE (FILTER_TAG, "Value: %f\n", value);
+
+    DEBUG_VERBOSE (FILTER_TAG, "Raw values:");
+	for (int i = 0; i < _order; i++) {
+        DEBUG_VERBOSE (FILTER_TAG, " %f", _rawValues[i]);
+	}
+
+    DEBUG_VERBOSE (FILTER_TAG, "Coeffs:");
+	for (int i = 0; i < _order; i++) {
+        DEBUG_VERBOSE (FILTER_TAG, " %f", _weightValues[i]);
+	}
+
+	if (_index < _order) {
+		_index++;
+		left = _order - _index;
+		right = _order - 1;
+	} 	else {
+		left = 0;
+		right = _order - 1;
+	}
+    DEBUG_VERBOSE (FILTER_TAG, "Index: %d , left: %d , right: %d\n", _index, left, right);
+
+	for (int i = left; i <= right; i++) {
+		sumValue += _rawValues[i] * _weightValues[i];
+		sumWeight += _weightValues[i];
+		//DBG_OUTPUT_PORT.printf("Raw value %d: %f\n", (i + 1), _rawValues[_order - (i+1)]);
+	}
+    DEBUG_VERBOSE (FILTER_TAG, "Sum: %f", sumValue);
+    DEBUG_VERBOSE (FILTER_TAG, " SumWeight: %f\n", sumWeight);
+
+	procValue = sumValue / sumWeight;
+
+    DEBUG_VERBOSE (FILTER_TAG, "Average: %f\n", procValue);
+
+	return procValue;
+}
+
+int FilterClass::divide (float* array, int start, int end) {
+	int left;
+	int right;
+	float pivot;
+	float temp;
+
+	pivot = array[start];
+	left = start;
+	right = end;
+
+	// While indexes do not cross
+	while (left < right) {
+		while (array[right] > pivot) {
+			right--;
+		}
+
+		while ((left < right) && (array[left] <= pivot)) {
+			left++;
+		}
+
+		// If indexes have not crossed yet we continue doing exchanges
+		if (left < right) {
+			temp = array[left];
+			array[left] = array[right];
+			array[right] = temp;
+		}
+	}
+
+	// Indexes have crossed. We put the pivot on place
+	temp = array[right];
+	array[right] = array[start];
+	array[start] = temp;
+
+	// NEw pivot position
+	return right;
+}
+
+void FilterClass::clear () {
+	for (int i = 0; i < _order; i++) {
+		_rawValues[i] = 0;
+		_orderedValues[i] = 0;
+		//_weightValues[i] = 1;
+	}
+	_index = 0;
+}
+
+FilterClass::~FilterClass () {
+	free (_rawValues);
+	free (_orderedValues);
+	free (_weightValues);
+}
+
+void FilterClass::quicksort (float* array, int start, int end) {
+	float pivot;
+
+	if (start < end) {
+		pivot = divide (array, start, end);
+
+		// Ordeno la lista de los menores
+		quicksort (array, start, pivot - 1);
+
+		// Ordeno la lista de los mayores
+		quicksort (array, pivot + 1, end);
+	}
+}
+
+float FilterClass::medianFilter (float value) {
+	float procValue;
+	int medianIdx;
+	int left, right, tempidx;
+	bool even;
+
+	if (_index < _order) {
+		_index++;
+		left = _order - _index;
+		right = _order - 1;
+		even = ((right - left) % 2) == 1;
+        DEBUG_VERBOSE (FILTER_TAG, "%d: ", (right - left) % 2);
+		if (even) {
+			tempidx = (right - left - 1) / 2;
+            DEBUG_VERBOSE (FILTER_TAG, "even\n");
+		} 		else {
+			tempidx = (right - left) / 2;
+            DEBUG_VERBOSE (FILTER_TAG, "odd\n");
+		}
+		medianIdx = right - _index + 1 + tempidx;
+	} 	else {
+		left = 0;
+		right = _order - 1;
+		even = (_order % 2) == 0;
+		if (even)
+			tempidx = (right - 1) / 2;
+		else
+			tempidx = right / 2;
+		medianIdx = right - _index + 1 + tempidx;
+	}
+    DEBUG_VERBOSE (FILTER_TAG, "Index: %d , left: %d , right: %d , even: %s , tempidx: %d , medianidx: %d\n", _index, left, right, (even ? "even" : "odd"), tempidx, medianIdx);
+
+	// Shift raw values
+	for (int i = 0; i < _order - 1; i++) {
+		_rawValues[i] = _rawValues[i + 1];
+	}
+	// Add new raw value
+	_rawValues[_order - 1] = value;
+
+    DEBUG_VERBOSE (FILTER_TAG, "Raw values:");
+	for (int i = 0; i < _order; i++) {
+        DEBUG_VERBOSE (FILTER_TAG, " %f", _rawValues[i]);
+	}
+
+	// copy to array before ordering
+	for (int i = 0; i < _order; i++) {
+		_orderedValues[i] = _rawValues[i];
+	}
+
+	// order values
+	quicksort (_orderedValues, left, right);
+
+    DEBUG_VERBOSE (FILTER_TAG, "Ordered values:");
+	for (int i = 0; i < _order; i++) {
+        DEBUG_VERBOSE (FILTER_TAG, " %f", _orderedValues[i]);
+	}
+
+	// select median value
+	if (!even) {
+		procValue = _orderedValues[medianIdx];
+	} 	else { // there is no center value
+		procValue = (_orderedValues[medianIdx] + _orderedValues[medianIdx + 1]) / 2.0F;
+	}
+
+    DEBUG_VERBOSE (FILTER_TAG, "Median: %f\n", procValue);
+	return procValue; // return mid value
+}
+
+FilterClass::FilterClass (FilterType_t type, uint8_t order) {
+	_filterType = type;
+
+	if (order < MAX_ORDER)
+		if (order > 1)
+			_order = order;
+		else
+			_order = MIN_ORDER;
+	else
+		_order = MAX_ORDER;
+
+	_rawValues = (float*)malloc (_order * sizeof (float));
+	for (int i = 0; i < _order; i++) {
+		_rawValues[i] = 0;
+	}
+
+	_orderedValues = (float*)malloc (_order * sizeof (float));
+	for (int i = 0; i < _order; i++) {
+		_orderedValues[i] = 0;
+	}
+
+	_weightValues = (float*)malloc (_order * sizeof (float));
+	for (int i = 0; i < _order; i++) {
+		_weightValues[i] = 1;
+	}
+
+}
+
---
+++ 13. Sensores y librerías/src/main.cpp
@@ -0,0 +1,213 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <sntp.h>
+#include <M5StickCPlus.h>
+#include <QuickDebug.h>
+#include <UNIT_ENV.h>
+#include "Filter.h"
+
+#if __has_include("wificonfig.h")
+#include "wificonfig.h"
+#else
+constexpr auto SSID = "SSID";
+constexpr auto PASSWORD = "PASSWORD";
+#endif
+
+// constexpr auto LED = 5;
+constexpr auto LED = 10;
+constexpr auto LED_ON = LOW;
+
+TaskHandle_t tareaLED = NULL;
+TaskHandle_t tareaMensaje = NULL;
+TimerHandle_t tareaDisplay = NULL;
+
+bool timeSyncd = false;
+
+TFT_eSprite Disbuff = TFT_eSprite (&M5.Lcd);
+float temp = -200;
+float hum = -1;
+float pressure = 0;
+
+float rate = 0;
+
+typedef enum ticker_state {
+    TEMPERATURA,
+    HUMEDAD,
+    PRESION,
+} ticker_state_t;
+
+ticker_state_t ticker = TEMPERATURA;
+
+const char* TAG = "SENSOR";
+
+SHT3X sht30;
+QMP6988 qmp6988;
+
+FilterClass tempFilter (MEDIAN_FILTER, 21);
+FilterClass humFilter (MEDIAN_FILTER, 21);
+FilterClass pressureFilter (MEDIAN_FILTER, 21);
+
+void updateDisplay (void* pvParameters) {
+    RTC_TimeTypeDef sTime;
+    constexpr auto periodoDisplay = 10;
+
+    static time_t lastDisplayUpdate = 0;
+    static time_t lastMeasurement = 0;
+    static float current;
+    static float voltage;
+    static String ssid;
+    static String localip;
+
+    if (millis () - lastMeasurement > 1000) {
+        lastMeasurement = millis ();
+        current = M5.Axp.GetBatCurrent ();
+        voltage = M5.Axp.GetBatVoltage ();
+        ssid = WiFi.SSID ();
+        localip = WiFi.localIP ().toString ();
+    }
+
+        //--------------------- Procesado visual -------------------------------------------
+    if (millis () - lastDisplayUpdate >= periodoDisplay) {
+        lastDisplayUpdate = millis ();
+        Disbuff.fillSprite (BLACK);
+        Disbuff.setCursor (10, 10);
+        Disbuff.setTextColor (WHITE);
+        Disbuff.setTextSize (4);
+        M5.Rtc.GetTime (&sTime);
+        Disbuff.printf ("%02d:%02d:%02d", sTime.Hours, sTime.Minutes, sTime.Seconds);
+        Disbuff.setCursor (10, 50);
+        Disbuff.setTextSize (2);
+        Disbuff.setTextColor (RED);
+        Disbuff.printf ("%.2f V %.2f mA", voltage, current);
+        Disbuff.setCursor (10, 75);
+        switch (ticker) {
+        case TEMPERATURA:
+            Disbuff.setTextSize (4);
+            if (temp > -100) {
+                Disbuff.setTextColor (GREEN);
+                Disbuff.printf ("T:%.2f C", temp);
+            } else {
+                Disbuff.setTextColor (RED);
+                Disbuff.printf ("T:--.-- C");
+            }
+            break;
+        case HUMEDAD:
+            Disbuff.setTextSize (4);
+            if (hum >= 0) {
+                Disbuff.setTextColor (GREEN);
+                Disbuff.printf ("H:%.2f %%", hum);
+            } else {
+                Disbuff.setTextColor (RED);
+                Disbuff.printf ("H:--.-- %%");
+            }
+            break;
+        default:
+            Disbuff.setTextSize (3);
+            if (pressure > 0) {
+                Disbuff.setTextColor (GREEN);
+                Disbuff.printf ("P:%.0f hPa", pressure);
+            } else {
+                Disbuff.setTextColor (RED);
+                Disbuff.printf ("P:----- hPa");
+            }
+            break;
+        }
+
+        Disbuff.pushSprite (0, 0);
+    }
+    //----------------------------------------------------------------------------------
+}
+
+void time_sync_cb (timeval* ntptime) {
+    time_t hora_actual = time (NULL);
+
+    tm* timeinfo = localtime (&hora_actual);
+    RTC_DateTypeDef sDate;
+    RTC_TimeTypeDef sTime;
+
+    sTime.Hours = timeinfo->tm_hour;
+    sTime.Minutes = timeinfo->tm_min;
+    sTime.Seconds = timeinfo->tm_sec;
+
+    M5.Rtc.SetTime (&sTime);
+
+    sDate.WeekDay = timeinfo->tm_wday;
+    sDate.Month = timeinfo->tm_mon + 1;
+    sDate.Date = timeinfo->tm_mday;
+    sDate.Year = timeinfo->tm_year + 1900;
+
+    M5.Rtc.SetData (&sDate);
+
+    timeSyncd = true;
+}
+
+void setup () {
+    Serial.begin (115200);
+    pinMode (LED, OUTPUT);
+    digitalWrite (LED, !LED_ON);
+
+    WiFi.mode (WIFI_STA);
+    WiFi.begin (SSID, PASSWORD);
+
+    M5.begin ();
+    M5.Axp.begin ();
+    Wire.begin ();
+    qmp6988.init ();
+    M5.Lcd.setRotation (3);
+    Disbuff.createSprite (240, 135);
+    Disbuff.setRotation (3);
+    Disbuff.fillSprite (BLACK);
+    Disbuff.setTextColor (WHITE);
+    Disbuff.setTextSize (2);
+    Disbuff.setCursor (10, 10);
+    Disbuff.printf ("Conectando");
+    Disbuff.pushSprite (0, 0);
+    delay (100);
+
+    while (WiFi.status () != WL_CONNECTED) {
+        delay (100);
+        Serial.print (".");
+    }
+    //WiFi.setAutoReconnect (false);
+
+
+    sntp_setoperatingmode (SNTP_OPMODE_POLL);
+    sntp_set_sync_mode (SNTP_SYNC_MODE_SMOOTH);
+    sntp_setservername (0, "192.168.5.120");
+    setenv ("TZ", PSTR ("CET-1CEST,M3.5.0,M10.5.0/3"), 1);
+    tzset ();
+    sntp_set_time_sync_notification_cb (time_sync_cb);
+    sntp_init ();
+
+    const uint fps = 10;
+
+    tareaDisplay = xTimerCreate ("Display", pdMS_TO_TICKS (1000 / fps), pdTRUE, NULL, updateDisplay);
+    xTimerStart (tareaDisplay, 0);
+}
+
+void loop () {
+    static time_t lastSensorUpdate = -290000;
+    const time_t sensorPeriod = 250;
+
+    if (millis () - lastSensorUpdate > sensorPeriod) {
+        lastSensorUpdate = millis ();
+        pressure = pressureFilter.addValue(qmp6988.calcPressure ());
+        if (sht30.get () == 0) {
+            temp = tempFilter.addValue(sht30.cTemp);
+            hum = humFilter.addValue(sht30.humidity);
+        } else {
+            temp = -200;
+            hum = -1;
+        }
+        DEBUG_INFO (TAG, "T:%.2f C H:%.2f %% P:%.0f hPa", temp, hum, pressure);
+    }
+
+    static time_t lastTickerChange = 0;
+    const time_t tickerPeriod = 1000 * 5;
+
+    if (millis () - lastTickerChange > tickerPeriod) {
+        lastTickerChange = millis ();
+        ticker = (ticker_state_t)((ticker + 1) % 3);
+    }
+
+}
